e2fbba235c3bdce3fcd22495b935a9260c180ce0
king-ai719 <receipic.jp@gmail.com>
2026-05-19 12:09:17 +0900

    fix: clerk-proxy edge runtime追加

--- src/app/api/clerk-proxy/[[...path]]/route.ts
+++ src/app/api/clerk-proxy/[[...path]]/route.ts
@@ -1,19 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server'
 
+export const runtime = 'edge'
+
 const CLERK_FRONTEND_API = 'https://frontend-api.clerk.services'
 
 export async function GET(req: NextRequest) {
-  const url = req.url.replace(
-    `${process.env.NEXT_PUBLIC_APP_URL}/api/clerk-proxy`,
-    CLERK_FRONTEND_API
-  )
-  return NextResponse.rewrite(url)
+  const { pathname, search } = new URL(req.url)
+  const path = pathname.replace('/api/clerk-proxy', '')
+  const target = `${CLERK_FRONTEND_API}${path}${search}`
+  return NextResponse.rewrite(target)
 }
 
 export async function POST(req: NextRequest) {
-  const url = req.url.replace(
-    `${process.env.NEXT_PUBLIC_APP_URL}/api/clerk-proxy`,
-    CLERK_FRONTEND_API
-  )
-  return NextResponse.rewrite(url)
+  const { pathname, search } = new URL(req.url)
+  const path = pathname.replace('/api/clerk-proxy', '')
+  const target = `${CLERK_FRONTEND_API}${path}${search}`
+  return NextResponse.rewrite(target)
 }
