36223c3198fca493c7fc24d3302e3eeea9023929
king-ai719 <receipic.jp@gmail.com>
2026-05-21 16:16:48 +0900

    feat: generate・dashboardページ追加

---
+++ src/app/(dashboard)/generate/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { PostForm } from '@/components/features/post-form/PostForm'
+
+export default function GeneratePage() {
+  return (
+    <div className="py-2">
+      <PostForm />
+    </div>
+  )
+}
